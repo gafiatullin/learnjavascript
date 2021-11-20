e1e275d9341ff8da5bdffaa95f52bcc35eeed4b4
Danis <56770883+gafiatullin@users.noreply.github.com>
2021-11-20 21:13:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+/*  let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Aboba";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+Valera zhostko puknuv
+let myFirstJob = 'MOKH';
+console.log(myFirstJob);
+*/
+
+/*let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'Aboba';
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+*/
+
+
